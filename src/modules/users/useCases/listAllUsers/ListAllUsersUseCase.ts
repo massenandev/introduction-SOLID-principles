192e9007831e8611731b350d73c38f3e89548c02
massenandev <massenandev@gmail.com>
2021-08-22 16:24:18 -0300

    feat(listAll): add controller and useCase

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists");
+    }
+
+    if (user.admin === false) {
+      throw new Error("Unauthorized");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,17 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists");
+    }
+
+    user.admin = true;
+
+    this.usersRepository.turnAdmin(user);
+
+    return user;
   }
 }
 
